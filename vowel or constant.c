c3645f09cc9dd5b2d8fd4ea70d1deddc3a0eb0a5
astha chaurasia <asthachaurasia10@gmail.com>
2026-02-16 22:02:35 +0530

    Create vowel or constant.c

---
+++ vowel or constant.c
@@ -0,0 +1,13 @@
+/*check vowel or constant*/
+#include<stdio.h>
+int main(){
+char ch;
+printf("enter character: ");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+printf("vowel\n");
+else
+printf("consonant\n");
+return 0;
+}
+
