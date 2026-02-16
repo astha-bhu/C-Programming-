973667a34180c082ab772c6453c8d32da3cecd35
astha chaurasia <asthachaurasia10@gmail.com>
2026-02-16 22:19:34 +0530

    Create swap two num call by reference.c

---
+++ swap two num call by reference.c
@@ -0,0 +1,17 @@
+/*swaping two numbers using call by reference*/
+#include<stdio.h>
+void swap(int *a,int *b);
+int main(){
+int x,y;
+printf("enter num1 & num2: ");
+scanf("%d %d",&x,&y);
+printf("before swaping x=%d y=%d\n",x,y);
+swap(&x,&y);
+printf("after swaping x=%d y=%d\n",x,y);
+return 0;
+}
+void swap(int *a,int *b){
+int temp=*a;
+*a=*b;
+*b=temp;
+}
